dbd167eeb53b1726fdb29b5197a9c1153cdab8f0
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-03-24 11:12:26 -0500

    simple pagination and handle if there is no author data provided in post

---
+++ frontend/src/app/blog/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import { fetchAPI } from "../utils/fetch-api";
+
+import Loader from "../components/Loader";
+import Blog from "../views/blog";
+
+export default function Profile() {
+  const [meta, setMeta] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const fetchData = useCallback(async (start, limit) => {
+    setLoading(true);
+    try {
+      const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+      const path = `/articles`;
+      const urlParamsObject = {
+        populate: {
+          cover: { fields: ["url"] },
+          authorsBio: {
+            populate: "*",
+          },
+        },
+        pagination: {
+          start: start,
+          limit: limit,
+        },
+      };
+      const options = { headers: { Authorization: `Bearer ${token}` } };
+      const responseData = await fetchAPI(path, urlParamsObject, options);
+
+      if (start === 0) {
+        setData(responseData.data);
+      } else {
+        setData(prevData => [...prevData, ...responseData.data]);
+      }
+
+      setMeta(responseData.meta);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  function loadMorePosts() {
+    const nextPosts = meta.pagination.start + meta.pagination.limit;
+    fetchData(nextPosts, 6);
+
+  }
+
+  useEffect(() => {
+    fetchData(0, 6);
+  }, [fetchData]);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <Blog data={data}>
+      {meta.pagination.start + meta.pagination.limit <
+        meta.pagination.total && (
+        <div className="flex justify-center">
+          <button
+            type="button"
+            className="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-900 dark:text-gray-400"
+            onClick={loadMorePosts}
+          >
+            Load more posts...
+          </button>
+        </div>
+      )}
+    </Blog>
+  );
+}
--- frontend/src/app/views/blog.tsx
+++ frontend/src/app/views/blog.tsx
@@ -35,51 +35,39 @@ interface Article {
   };
 }
 
-interface Meta {
-  pagination: {
-    page: number;
-    pageSize: number;
-    pageCount: number;
-    total: number;
-  };
-}
-
-interface BlogData {
+export default function Blog({
+  data: articles,
+  children,
+}: {
   data: Article[];
-  meta: Meta;
-}
-
-// data.data[0].attributes.cover.data.attributes.url
-
-export default function Blog({ data }: { data: BlogData }) {
-  const { data: articles, meta } = data;
+  children: React.ReactNode;
+}) {
   return (
     <section className="container p-6 mx-auto space-y-6 sm:space-y-12">
       <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {articles.map((article) => {
-          const authorsBio = article.attributes.authorsBio.data.attributes;
-          console.log(authorsBio, "############### blog posts ###############");
-
           const imageUrl = getStrapiMedia(
             article.attributes.cover.data?.attributes.url
           );
 
-          const { name, avatar } =
-            article.attributes.authorsBio.data.attributes;
-          const avatarUrl = getStrapiMedia(avatar.data.attributes.url);
+          const authorsBio = article.attributes.authorsBio.data?.attributes;
+
+          const avatarUrl = getStrapiMedia(
+            authorsBio?.avatar.data.attributes.url
+          );
 
           return (
             <Link
               href={`blog/${article.attributes.slug}`}
               key={article.id}
-              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 min-w-[300px]"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 min-w-[375px] rounded-2xl overflow-hidden shadow-lg"
             >
               {imageUrl && (
                 <Image
                   alt="presentation"
                   width="240"
                   height="240"
-                  className="object-cover w-full rounded h-44 "
+                  className="object-cover w-full h-44 "
                   src={imageUrl}
                 />
               )}
@@ -101,7 +89,11 @@ export default function Blog({ data }: { data: BlogData }) {
                   <span className="text-xs dark:text-gray-400">
                     {formatDate(article.attributes.publishedAt)}
                   </span>
-                  <span className="text-xs dark:text-gray-400">{name}</span>
+                  {authorsBio && (
+                    <span className="text-xs dark:text-gray-400">
+                      {authorsBio.name}
+                    </span>
+                  )}
                 </div>
                 <p className="py-4">{article.attributes.description}</p>
               </div>
@@ -109,14 +101,7 @@ export default function Blog({ data }: { data: BlogData }) {
           );
         })}
       </div>
-      <div className="flex justify-center">
-        <button
-          type="button"
-          className="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-900 dark:text-gray-400"
-        >
-          Load more posts...
-        </button>
-      </div>
+      {children}
     </section>
   );
 }
